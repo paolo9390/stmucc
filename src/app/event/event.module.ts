82dd361b937cdcb90dc6ef36ad8d1ec3e18c97f7
Paolo Baskharoun <paolo9390@gmail.com>
2019-11-05 12:28:17 +0000

    updated event module

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventComponent } from './event.component';
-import { MatDialogModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatExpansionModule, MatDividerModule, MatCardModule, MatListModule, MatSelectModule } from '@angular/material';
+import { MatDialogModule, MatButtonModule, MatIconModule, MatExpansionModule, MatDividerModule, MatCardModule, MatListModule } from '@angular/material';
 import { EventDialogComponent } from './event-dialog/event-dialog.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TitleModule } from '../_shared/title/title.module';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EventComponent, EventDialogComponent],
@@ -15,17 +14,12 @@ import { FormsModule } from '@angular/forms';
     MatDialogModule,
     MatButtonModule,
     MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
     MatExpansionModule,
     FlexLayoutModule,
     MatDividerModule,
     MatCardModule,
     TitleModule,
     MatListModule,
-    MatInputModule,
-    FormsModule,
     RouterModule
   ],
   exports: [
